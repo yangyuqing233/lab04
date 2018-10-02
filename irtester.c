efd1558667b5dea9e9de6f7421bbdfe2676277ab
Yangyuqing233 <1501172184y@gmail.com>
2018-10-02 18:36:49 +0000

    “setup”

--- irtester.c
+++ irtester.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-int main(int argc, char *argv[])
-{
-  int i;
+int main(int argc, char *argv[]){
+int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
   while(1) {
-    printf("Waiting for reset\n");
-    while(digitalRead(0) == 0){
-    printf("Got a 0\n");
-    digitalWrite(2,LOW);
-    digitalWrite(1,HIGH);}
-    printf("Waiting for event\n");
-    if(digitalRead(0) == 1){
-    while(digitalRead(0) == 1){
+    printf("waitting for reset\n");
+    while (digitalRead(0)==1){
     printf("Got a 1\n");
     digitalWrite(2,HIGH);
+    digitalWrite(1,LOW);
+    ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "george1", "EECS32222", "Hii George1 1234656");
+}
+   printf("waitting for event\n");
+    if(digitalRead(0) == 0){
+    printf("Got a 0\n");
+    digitalWrite(2,LOW);
+    digitalWrite(1,HIGH);delay(200);
     digitalWrite(1,LOW);}
     printf("Alarm\n");
   }
   /*NOTREACHED*/
-  return 0 ;
+return 0 ;
 }
--- tester.c
+++ tester.c
@@ -8,6 +8,6 @@ int main(int argc, char *argv[])
 */
 
   printf("Trying to connect to server\n");
-  ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "my1", "my 2", "my 33333");
+  ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "Tom", "11111", "mybad 33333");
   return 0;
 }
