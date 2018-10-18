5450e99332962d80da5d5ae0336d06c60b0fc0b2
yangyuqing233 <43785074+yangyuqing233@users.noreply.github.com>
2018-10-18 14:40:46 -0400

    Add files via upload

--- irtester.c
+++ irtester.c
@@ -10,17 +10,18 @@ int main(int argc, char *argv[])
   pinMode(2, OUTPUT);
   while(1) {
     printf("Waiting for reset\n");
+    
     while(digitalRead(0) == 0){
-    printf("Got a 0\n");
-    digitalWrite(2,LOW);
-    digitalWrite(1,HIGH);}
     printf("Waiting for event\n");
-    if(digitalRead(0) == 1){
+    digitalWrite(1, HIGH); delay(500);
+    digitalWrite(2, LOW);
+	}
+    
     while(digitalRead(0) == 1){
-    printf("Got a 1\n");
-    digitalWrite(2,HIGH);
-    digitalWrite(1,LOW);}
     printf("Alarm\n");
+    digitalWrite (2, HIGH); delay(500);
+    digitalWrite (1, LOW);
+  	}
   }
   /*NOTREACHED*/
   return 0 ;
